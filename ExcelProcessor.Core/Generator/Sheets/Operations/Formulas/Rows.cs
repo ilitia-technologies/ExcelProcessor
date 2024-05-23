f4b31b367696ebd931262250b96982aa7d36bf4c
Francisco <francisco@ilitia.com>
2024-05-23 10:40:28 +0200

    Rows formula

---
+++ ExcelProcessor.Core/Generator/Sheets/Operations/Formulas/Rows.cs
@@ -0,0 +1,21 @@
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using ExcelProcessor.Abstractions.Pointers;
+
+namespace ExcelProcessor.Core.Generator.Sheets.Operations.Formulas
+{
+    public class Rows : Formula
+    {
+        private readonly ICellReference from;
+        private readonly ICellReference to;
+        public Rows(ICellReference from, ICellReference to)
+        {
+            this.from = from;
+            this.to = to;
+        }
+
+        public override CellFormula Build()
+        {
+            return new CellFormula($"ROWS({from.ToExcelString()}{":"}{to.ToExcelString()})");
+        }
+    }
+}
